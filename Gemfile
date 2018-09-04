403450152f683a69599f414e9b2f97be83f7fb4c
Olorunnisola Tosin <77tosin@gmail.com>
2018-09-04 13:25:46 +0000

    I added twitter boostrap using SASS gem, also included Navbar

--- Gemfile
+++ Gemfile
@@ -3,6 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.0'
 
+#Ui for Twitter 
+
+gem 'bootstrap-sass', '~> 3.3.7'
+
+gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  
+  #define root url
+  root 'pages#index'
+  
+ 
+  get '/home' => 'pages#home'
+  get '/profile' => 'pages#profile'
+  get '/explore' => 'pages#explore'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
